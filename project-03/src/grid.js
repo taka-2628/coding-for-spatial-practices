2208c66cff6042b61f80dab5c002887c3ac34292
Takashi <th2826@columbia.edu>
2022-05-02 23:52:46 -0400

    project-03 update: js files separated

---
+++ project-03/src/grid.js
@@ -0,0 +1,41 @@
+
+
+function renderGrid(dataset) {
+  for (let i = 0; i < dataset.length; i++){
+    
+    const card = document.createElement("a");
+    card.classList.add("card");
+
+    const image = document.createElement("img");
+    image.setAttribute("src", dataset[i].IMAGE);
+
+    const blackOverlay = document.createElement("div");
+    blackOverlay.classList.add("black");
+
+    const hover = document.createElement("div");
+    hover.classList.add("hover");
+
+    const text = document.createElement("div");
+    text.classList.add("text")
+
+    const name = document.createElement("p");
+    name.classList.add("name");
+    name.textContent = dataset[i].NAME;
+    const latLong = document.createElement("span");
+    latLong.classList.add("lat-long");
+    latLong.textContent = `(${dataset[i].LATITUDE}, ${dataset[i].LONGITUDE})`
+
+    text.appendChild(name);
+    text.appendChild(latLong);
+    
+    hover.appendChild(text)
+    
+    card.appendChild(image)
+    card.appendChild(blackOverlay)
+    card.appendChild(hover)
+    
+    cardContainer.appendChild(card);
+  }
+}
+
+renderGrid(dataset);
---
+++ project-03/src/map.js
@@ -0,0 +1,15 @@
+mapButton.addEventListener("click", () => {
+  hideGrid();
+  showMap();
+})
+
+function hideGrid(){
+  gridView.style.display = "none";
+}
+
+function showMap(){
+  mapView.style.display = "block";
+}
+
+
+/* mapbox://styles/th-th/cl2pjz7ll000915of2t12waa2 */
